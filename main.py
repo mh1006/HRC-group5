ee2b83e5fb92563227cb48eca5c0eac93fb1a2ba
LoudMines <luitmeinen@gmail.com>
2026-04-21 14:45:01 +0200

    First version of the dialogflow connected, can only detect intent from strings

--- main.py
+++ main.py
@@ -1,5 +1,39 @@
 import os
 from google.cloud import dialogflow
 
-credential_path = (r'[ee-luitmeinen-859f52034670.json]')
+credential_path = (r'project-631e036d-75af-4f9e-b4b-44751f4edea7.json')
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
+
+def detect_intent_texts(project_id, session_id, texts, language_code):
+    """Returns the result of detect intent with texts as inputs.
+
+    Using the same `session_id` between requests allows continuation
+    of the conversation."""
+
+    session_client = dialogflow.SessionsClient()
+
+    session = session_client.session_path(project_id, session_id)
+    print("Session path: {}\n".format(session))
+
+    for text in texts:
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+
+        print("=" * 20)
+        print("Query text: {}".format(response.query_result.query_text))
+        print(
+            "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+            )
+        )
+        print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+
+if __name__ == '__main__':
+    detect_intent_texts('project-631e036d-75af-4f9e-b4b','3',["test"], "en-UK")
+
